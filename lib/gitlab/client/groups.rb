c25f9ba85d731c1ed4dc3d1112fa20b458344be4
Nihad Abbasov <narkoz.2008@gmail.com>
2013-04-06 16:15:04 +0500

    remove unnecessary code

--- lib/gitlab/client/groups.rb
+++ lib/gitlab/client/groups.rb
@@ -1,7 +1,6 @@
 class Gitlab::Client
   # Defines methods related to groups.
   module Groups
-
     def groups
       get("/groups")
     end
@@ -10,7 +9,7 @@ class Gitlab::Client
       get("/groups/#{group_id}")
     end
 
-    #Creates a new group
+    # Creates a new group
     #
     # @param  [String] name
     # @param  [String] path
@@ -19,7 +18,7 @@ class Gitlab::Client
       post("/groups", :body => body)
     end
 
-    #Transfers a project to a group
+    # Transfers a project to a group
     #
     # @param  [Integer] group_id
     # @param  [Integer] project_id
@@ -27,25 +26,5 @@ class Gitlab::Client
       body = {:id => group_id, :project_id => project_id}
       post("/groups/#{group_id}/projects/#{project_id}", :body => body)
     end
-
-
-    # Creates a new issue.
-    #
-    # @param  [Integer, String] project The ID or code name of a project.
-    # @param  [String] title The title of an issue.
-    # @param  [Hash] options A customizable set of options.
-    # @option options [String] :description The description of an issue.
-    # @option options [Integer] :assignee_id The ID of a user to assign issue.
-    # @option options [Integer] :milestone_id The ID of a milestone to assign issue.
-    # @option options [String] :labels Comma-separated label names for an issue.
-    # @return [Gitlab::ObjectifiedHash] Information about created issue.
-=begin
-    def create_issue(project, title, options={})
-      body = {:title => title}.merge(options)
-      post("/projects/#{project}/issues", :body => body)
-    end
-=end
-
-
   end
 end
