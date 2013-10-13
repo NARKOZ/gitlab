2ef4282bcf3480f06c49967fd37f9ac8a556a840
Nihad Abbasov <narkoz.2008@gmail.com>
2013-10-13 17:43:03 +0500

    fix docs

--- lib/gitlab/client/groups.rb
+++ lib/gitlab/client/groups.rb
@@ -41,7 +41,7 @@ class Gitlab::Client
     # @example
     #   Gitlab.group_members(1)
     #
-    # @param  [Integer] (required) - The ID of a group
+    # @param  [Integer] id The ID of a group.
     # @return [Gitlab::ObjectifiedHash]
     def group_members(id)
       get("/groups/#{id}/members")
@@ -52,10 +52,10 @@ class Gitlab::Client
     # @example
     #   Gitlab.add_group_member(1, 2, 40)
     #
-    # @param  [Integer] (required) The group id to add a member to
-    # @param  [Integer] (required) The user id of the user to add to the team
-    # @param  [Integer] (required) access_level Project access level
-    # @return [Array<Gitlab::ObjectifiedHash>] Information about added team member.
+    # @param  [Integer] team_id The group id to add a member to.
+    # @param  [Integer] user_id The user id of the user to add to the team.
+    # @param  [Integer] access_level Project access level.
+    # @return [Gitlab::ObjectifiedHash] Information about added team member.
     def add_group_member(team_id, user_id, access_level)
       post("/groups/#{team_id}/members", :body => {:user_id => user_id, :access_level => access_level})
     end
@@ -65,9 +65,9 @@ class Gitlab::Client
     # @example
     #   Gitlab.remove_group_member(1, 2)
     #
-    # @param  [Integer] (required) The group ID.
-    # @param  [Integer] (required) id The ID of a user.
-    # @return [Array<Gitlab::ObjectifiedHash>] Information about removed team member.
+    # @param  [Integer] team_id The group ID.
+    # @param  [Integer] user_id The ID of a user.
+    # @return [Gitlab::ObjectifiedHash] Information about removed team member.
     def remove_group_member(team_id, user_id)
       delete("/groups/#{team_id}/members/#{user_id}")
     end
