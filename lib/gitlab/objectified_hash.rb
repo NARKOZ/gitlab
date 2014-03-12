b9981ccc8b85c8503ad9ba53ef4f1f75e355d9ac
Christian Höltje <docwhat@gerf.org>
2014-03-12 18:38:45 -0400

    Enhanced ObjectifiedHash with Hashie
    This should make the `ObjectifiedHash` behave in a more
    regular fashion.  Some examples:
    
    * `objhash['name'] == objhash[:name] == objhash.name`
    * `objhash.name?` is `true` if the value of `objhash[:name]` is truthy.
    * `objhash.keys` returns the list of keys on the hash.
    
    In addition, there are now tests for `ObjectifiedHash`.

--- lib/gitlab/objectified_hash.rb
+++ lib/gitlab/objectified_hash.rb
@@ -1,18 +1,43 @@
+require 'hashie/extensions/coercion'
+require 'hashie/extensions/indifferent_access'
+require 'hashie/extensions/method_access'
+
 module Gitlab
   # Converts hashes to the objects.
-  class ObjectifiedHash
-    # Creates a new ObjectifiedHash.
+  class ObjectifiedHash < ::Hash
+    include ::Hashie::Extensions::Coercion
+    include ::Hashie::Extensions::IndifferentAccess
+    include ::Hashie::Extensions::MethodReader
+    include ::Hashie::Extensions::MethodQuery
+
+    coerce_value ::Hash, ObjectifiedHash
+
     def initialize(hash)
-      @data = hash.inject({}) do |data, (key,value)|
-        value = ObjectifiedHash.new(value) if value.is_a? Hash
-        data[key.to_s] = value
-        data
+      hash.each_pair do |key, value|
+        self[key] = _convert_value value
       end
     end
 
-    # Delegate to ObjectifiedHash
-    def method_missing(key)
-      @data.key?(key.to_s) ? @data[key.to_s] : nil
+    def key(value=:unset)
+      if self[:key]
+        raise ArgumentError, 'wrong number of arguments (1 for 0)' if value != :unset
+        self[:key]
+      else
+        super
+      end
+    end
+
+    def _convert_value(value)
+      case value
+      when self.class
+        value.dup
+      when ::Hash
+        self.class.new(value.dup)
+      when ::Enumerable
+        value.collect{ |e| _convert_value(e) }
+      else
+        value
+      end
     end
   end
 end
