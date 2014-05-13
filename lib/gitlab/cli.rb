3f01f0d6eb321dbf903b4039d111ba3c2cdc927e
Nihad Abbasov <narkoz.2008@gmail.com>
2014-05-14 02:43:08 +0500

    output data in a table

--- lib/gitlab/cli.rb
+++ lib/gitlab/cli.rb
@@ -11,14 +11,14 @@ class Gitlab::CLI
     when 'help'
       puts 'Available commands:'
       puts '-' * 19
-      puts Gitlab.actions.join(', ')
+      puts Gitlab.actions.sort.join(', ')
     when '-v', '--version'
       puts "Gitlab Ruby Gem #{Gitlab::VERSION}"
     else
       if Gitlab.actions.include?(cmd.to_sym)
         begin
           data = arguments.any? ? Gitlab.send(cmd.to_sym, *arguments) : Gitlab.send(cmd.to_sym)
-          puts data.to_h
+          output_styled_hash(data.to_h, cmd, arguments)
         rescue => e
           puts e.message
           exit(1)
@@ -26,6 +26,42 @@ class Gitlab::CLI
       else
         puts 'Unknown command'
         exit(1)
+      end
+    end
+  end
+
+  def self.output_styled_hash(hash, cmd, args)
+    keys = hash.keys.sort {|x, y| x.to_s <=> y.to_s }
+    max_key_length = hash.keys.map {|k| k.to_s.size }.max
+    max_value_length = hash.values.map {|v| v.to_s.size }.max
+
+    keys.each_with_index do |key, index|
+      case value = hash[key]
+      when Array
+        # TODO: handle
+        next
+      when Hash
+        # TODO: handle
+        next
+      when nil
+        next
+      else
+        separator = '+' + '-' * (max_value_length + max_key_length + 5) + '+'
+
+        # print executed command
+        if index == 1
+          puts separator
+          print "| ".ljust(max_key_length)
+          print "Gitlab.#{cmd} #{args.join(', ')}"
+          puts ' |'.rjust(max_value_length) # FIXME
+        end
+
+        # print returned data
+        puts separator
+        print "| #{key}".ljust(max_key_length + 3)
+        print "| #{value}"
+        puts ' |'.rjust(max_value_length - value.to_s.size + 2)
+        puts separator if keys.size - 1 == index
       end
     end
   end
