6b344d46a4c023b36aad3a9cb66548745158a3c4
Nihad Abbasov <narkoz.2008@gmail.com>
2014-05-15 15:25:22 +0500

    use terminal-table for CLI output

--- lib/gitlab/cli.rb
+++ lib/gitlab/cli.rb
@@ -1,4 +1,5 @@
 require 'gitlab'
+require 'terminal-table/import'
 
 class Gitlab::CLI
   def self.start(args)
@@ -18,7 +19,7 @@ class Gitlab::CLI
       if Gitlab.actions.include?(cmd.to_sym)
         begin
           data = arguments.any? ? Gitlab.send(cmd.to_sym, *arguments) : Gitlab.send(cmd.to_sym)
-          output_styled_hash(data.to_h, cmd, arguments)
+          puts table_output(data.to_h, cmd, arguments)
         rescue => e
           puts e.message
           exit(1)
@@ -30,40 +31,26 @@ class Gitlab::CLI
     end
   end
 
-  def self.output_styled_hash(hash, cmd, args)
+  def self.table_output(hash, cmd, args)
     keys = hash.keys.sort {|x, y| x.to_s <=> y.to_s }
-    max_key_length = hash.keys.map {|k| k.to_s.size }.max
-    max_value_length = hash.values.map {|v| v.to_s.size }.max
 
-    keys.each_with_index do |key, index|
-      separator = '+' + '-' * (max_value_length + max_key_length + 5) + '+'
+    table do |t|
+      t.title = "Gitlab.#{cmd} #{args.join(', ')}"
 
-      # print executed command
-      if index == 0
-        puts separator
-        print "| ".ljust(max_key_length)
-        print "Gitlab.#{cmd} #{args.join(', ')}"
-        puts ' |'.rjust(max_value_length) # FIXME
-      end
+      keys.each_with_index do |key, index|
+        case value = hash[key]
+        when Array
+          # TODO
+          next
+        when Hash
+          # TODO
+          next
+        when nil
+          value = 'null'
+        end
 
-      case value = hash[key]
-      when Array
-        # TODO: handle
-        next
-      when Hash
-        # TODO: handle
-        next
-      when nil
-        next
-      else
-
-
-        # print returned data
-        puts separator
-        print "| #{key}".ljust(max_key_length + 3)
-        print "| #{value}"
-        puts ' |'.rjust(max_value_length - value.to_s.size + 2)
-        puts separator if keys.size - 1 == index
+        t.add_row [key, value]
+        t.add_separator unless keys.size - 1 == index
       end
     end
   end
