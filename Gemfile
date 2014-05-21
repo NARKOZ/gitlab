ab60d189617c49b45fb73f04b0bb2cc0a2683c63
Nihad Abbasov <narkoz.2008@gmail.com>
2014-05-21 17:36:40 +0500

    update configuration

--- Gemfile
+++ Gemfile
@@ -5,6 +5,4 @@ gem 'middleman', '~> 3.3.2'
 gem 'slim'
 gem 'bootstrap-sass', '~> 3.1.1'
 
-group :development do
-  gem 'middleman-livereload', '~> 3.1.0'
-end
+gem 'middleman-livereload', '~> 3.1.0'
--- config.rb
+++ config.rb
@@ -1,13 +1,4 @@
 ###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
 # Page options, layouts, aliases and proxies
 ###
 
@@ -33,12 +24,12 @@
 ###
 
 # Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -48,24 +39,22 @@
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
