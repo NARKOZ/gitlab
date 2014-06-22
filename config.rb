2ec2ea5c38d766785b919812a0d56158f943b8ec
Nihad Abbasov <narkoz.2008@gmail.com>
2014-06-22 23:47:02 +0500

    drop html extension from pages

--- config.rb
+++ config.rb
@@ -1,28 +1,11 @@
 Slim::Engine.set_default_options format: :html5
 
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+%w(cli configuration examples installation usage).each do |page|
+  proxy page, "#{page}.html"
+end
 
 ###
-# Helpers
+# Deploy
 ###
 
 commit_sha = `git log --pretty="%h" -n1`.strip
@@ -44,12 +27,15 @@ configure :development do
   Slim::Engine.set_default_options pretty: true
 end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+###
+# Helpers
+###
+
+helpers do
+  def link_to_get_started
+    link_to 'Get Started', 'installation', class: 'btn btn-primary btn-block'
+  end
+end
 
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
