ffcb07b39b686d1ca7fcbc42fae961c9923d31d4
Marc Siegfriedt <marc@techaccelerator.com>
2015-03-05 17:07:46 -0800

    added the rest of the commits functions added tests

--- lib/gitlab/client/commits.rb
+++ lib/gitlab/client/commits.rb
@@ -5,6 +5,7 @@ class Gitlab::Client
     #
     # @example
     #   Gitlab.commits(42)
+    #   Gitlab.repo_commits(5)
     #
     # @param  [Integer] project The ID of a project.
     # @option options [String] :ref_name The branch / tag name, default branch if nil.
@@ -16,73 +17,67 @@ class Gitlab::Client
     end
     alias_method :repo_commits, :commits
 
-
-
-
-
-
-
-
-
-
-
-    # Gets information about a repository branch.
+    # Gets information about a repository commit.
     #
     # @example
-    #   Gitlab.branch(3, 'api')
-    #   Gitlab.repo_branch(5, 'master')
+    #   Gitlab.commit(3, '6104942438c14ec7bd21c6cd5bd995272b3faff6')
+    #   Gitlab.repo_commit(5, '6104942438c14ec7bd21c6cd5bd995272b3faff6')
     #
     # @param  [Integer] project The ID of a project.
-    # @param  [String] branch The name of the branch.
+    # @param  [String] sha The sha of the commit.
     # @return [Gitlab::ObjectifiedHash]
-    # def branch(project, branch)
-    #   get("/projects/#{project}/repository/branches/#{branch}")
-    # end
+    def commit(project, sha)
+      get("/projects/#{project}/repository/commits/#{sha}")
+    end
+    alias_method :repo_commit, :commit
 
-    # alias_method :repo_branch, :branch
+    # Gets diff info for a repository commit.
+    #
+    # @example
+    #   Gitlab.commit_diff(3, '6104942438c14ec7bd21c6cd5bd995272b3faff6')
+    #   Gitlab.repo_commit_diff(5, '6104942438c14ec7bd21c6cd5bd995272b3faff6')
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [String] sha The sha of the commit.
+    # @return [Gitlab::ObjectifiedHash]
+    def commit_diff(project, sha)
+      get("/projects/#{project}/repository/commits/#{sha}/diff")
+    end
+    alias_method :repo_commit_diff, :commit_diff
 
-    # # Protects a repository branch.
-    # #
-    # # @example
-    # #   Gitlab.protect_branch(3, 'api')
-    # #   Gitlab.repo_protect_branch(5, 'master')
-    # #
-    # # @param  [Integer] project The ID of a project.
-    # # @param  [String] branch The name of the branch.
-    # # @return [Gitlab::ObjectifiedHash]
-    # def protect_branch(project, branch)
-    #   put("/projects/#{project}/repository/branches/#{branch}/protect")
-    # end
-    # alias_method :repo_protect_branch, :protect_branch
+    # Get the comments of a commit.
+    #
+    # @example
+    #   Gitlab.commit_comments(3, '6104942438c14ec7bd21c6cd5bd995272b3faff6')
+    #   Gitlab.repo_commit_comments(5, '6104942438c14ec7bd21c6cd5bd995272b3faff6')
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [String] sha The sha of the commit.
+    # @option options [Integer] :page The page number.
+    # @option options [Integer] :per_page The number of results per page.
+    # @return [Gitlab::ObjectifiedHash]
+    def commit_comments(project, sha, options={})
+      get("/projects/#{project}/repository/commits/#{sha}/comments", query: options)
+    end
+    alias_method :repo_commit_comments, :commit_comments
 
-    # # Unprotects a repository branch.
-    # #
-    # # @example
-    # #   Gitlab.unprotect_branch(3, 'api')
-    # #   Gitlab.repo_unprotect_branch(5, 'master')
-    # #
-    # # @param  [Integer] project The ID of a project.
-    # # @param  [String] branch The name of the branch.
-    # # @return [Gitlab::ObjectifiedHash]
-    # def unprotect_branch(project, branch)
-    #   put("/projects/#{project}/repository/branches/#{branch}/unprotect")
-    # end
-    # alias_method :repo_unprotect_branch, :unprotect_branch
-
-    # # Creates a repository branch.  Requires Gitlab >= 6.8.x
-    # #
-    # # @example
-    # #   Gitlab.create_branch(3, 'api')
-    # #   Gitlab.repo_create_branch(5, 'master')
-    # #
-    # # @param  [Integer] project The ID of a project.
-    # # @param  [String] branch The name of the new branch.
-    # # @param  [String] ref Create branch from commit sha or existing branch
-    # # @return [Gitlab::ObjectifiedHash]
-    # def create_branch(project, branch, ref)
-    #   post("/projects/#{project}/repository/branches",:body => {:branch_name => branch, :ref => ref})
-    # end
-    # alias_method :repo_create_branch, :create_branch
+    # Post comment to a commit.
+    #
+    # @example
+    #   Gitlab.create_commit_comment(3, '6104942438c14ec7bd21c6cd5bd995272b3faff6', 'comment')
+    #   Gitlab.repo_create_commit_comment(5, '6104942438c14ec7bd21c6cd5bd995272b3faff6', 'comment')
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [String] sha The sha of the commit.
+    # @param  [String] comment The comment to add.
+    # @option options [String] :path Teh file path.
+    # @option options [Integer] :line The line number.
+    # @option options [Integer] :line_type The line type (new or old).
+    # @return [Gitlab::ObjectifiedHash]
+    def create_commit_comment(project, sha, comment, options={})
+      post("/projects/#{project}/repository/commits/#{sha}/comments", body: { note: comment }.merge(options))
+    end
+    alias_method :repo_create_commit_comment, :create_commit_comment
 
   end
 end
