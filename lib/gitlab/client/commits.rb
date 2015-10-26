fc34acb6b475bb2555e4b035c11fdf42b3db4085
Sean Edge <asedge@gmail.com>
2015-10-26 18:55:50 -0400

    Add Gitlab::Client::Commits and rearrange methods and tests related to commits.

---
+++ lib/gitlab/client/commits.rb
@@ -0,0 +1,121 @@
+class Gitlab::Client
+  # Defines methods related to repository commits.
+  # @see https://github.com/gitlabhq/gitlabhq/blob/master/doc/api/commits.md
+  module Commits
+    # Gets a list of project commits.
+    #
+    # @example
+    #   Gitlab.commits('viking')
+    #   Gitlab.repo_commits('gitlab', :ref_name => 'api')
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [Hash] options A customizable set of options.
+    # @option options [String] :ref_name The branch or tag name of a project repository.
+    # @option options [Integer] :page The page number.
+    # @option options [Integer] :per_page The number of results per page.
+    # @return [Array<Gitlab::ObjectifiedHash>]
+    def commits(project, options={})
+      get("/projects/#{project}/repository/commits", :query => options)
+    end
+    alias_method :repo_commits, :commits
+
+    # Gets a specific commit identified by the commit hash or name of a branch or tag.
+    #
+    # @example
+    #   Gitlab.commit(42, '6104942438c14ec7bd21c6cd5bd995272b3faff6')
+    #   Gitlab.repo_commit(3, 'ed899a2f4b50b4370feeea94676502b42383c746')
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [String] sha The commit hash or name of a repository branch or tag
+    # @return [Gitlab::ObjectifiedHash]
+    def commit(project, sha)
+      get("/projects/#{project}/repository/commits/#{sha}")
+    end
+    alias_method :repo_commit, :commit
+
+    # Get the diff of a commit in a project.
+    #
+    # @example
+    #   Gitlab.commit_diff(42, '6104942438c14ec7bd21c6cd5bd995272b3faff6')
+    #   Gitlab.repo_commit_diff(3, 'ed899a2f4b50b4370feeea94676502b42383c746')
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [String] sha The name of a repository branch or tag or if not given the default branch.
+    # @return [Gitlab::ObjectifiedHash]
+    def commit_diff(project, sha)
+      get("/projects/#{project}/repository/commits/#{sha}/diff")
+    end
+    alias_method :repo_commit_diff, :commit_diff
+
+    # Gets a list of comments for a commit.
+    #
+    # @example
+    #   Gitlab.commit_comments(5, 'c9f9662a9b1116c838b523ed64c6abdb4aae4b8b')
+    #
+    # @param [Integer] project The ID of a project.
+    # @param [String] sha The commit hash or name of a repository branch or tag.
+    # @option options [Integer] :page The page number.
+    # @option options [Integer] :per_page The number of results per page.
+    # @return [Array<Gitlab::ObjectifiedHash>]
+    def commit_comments(project, commit, options={})
+      get("/projects/#{project}/repository/commits/#{commit}/comments", :query => options)
+    end
+    alias_method :repo_commit_comments, :commit_comments
+
+    # Creates a new comment for a commit.
+    #
+    # @example
+    #   Gitlab.create_commit_comment(5, 'c9f9662a9b1116c838b523ed64c6abdb4aae4b8b', 'Nice work on this commit!')
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [String] sha The commit hash or name of a repository branch or tag.
+    # @param  [String] note The text of a comment.
+    # @param  [Hash] options A customizable set of options.
+    # @option options [String] :path The file path.
+    # @option options [Integer] :line The line number.
+    # @option options [String] :line_type The line type (new or old).
+    # @return [Gitlab::ObjectifiedHash] Information about created comment.
+    def create_commit_comment(project, commit, note, options={})
+      post("/projects/#{project}/repository/commits/#{commit}/comments", :body => options.merge(:note => note))
+    end
+    alias_method :repo_create_commit_comment, :create_commit_comment
+
+    # Get the status of a commit
+    #
+    # @example
+    #   Gitlab.commit_status(42, '6104942438c14ec7bd21c6cd5bd995272b3faff6')
+    #   Gitlab.commit_status(42, '6104942438c14ec7bd21c6cd5bd995272b3faff6', name: 'jenkins')
+    #   Gitlab.commit_status(42, '6104942438c14ec7bd21c6cd5bd995272b3faff6', name: 'jenkins', all: true)
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [String] sha The commit hash
+    # @param  [Hash] options A customizable set of options.
+    # @option options [String] :ref Filter by ref name, it can be branch or tag
+    # @option options [String] :stage Filter by stage
+    # @option options [String] :name Filer by status name, eg. jenkins
+    # @option options [Boolean] :all The flag to return all statuses, not only latest ones
+    def commit_status(id, sha, options = {})
+      get("/projects/#{id}/repository/commits/#{sha}/statuses", query: options)
+    end
+    alias_method :repo_commit_status, :commit_status
+
+    # Adds or updates a status of a commit.
+    #
+    # @example
+    #   Gitlab.update_commit_status(42, '6104942438c14ec7bd21c6cd5bd995272b3faff6', 'success')
+    #   Gitlab.update_commit_status(42, '6104942438c14ec7bd21c6cd5bd995272b3faff6', 'failed', name: 'jenkins')
+    #   Gitlab.update_commit_status(42, '6104942438c14ec7bd21c6cd5bd995272b3faff6', 'canceled', name: 'jenkins', target_url: 'http://example.com/builds/1')
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [String] sha The commit hash
+    # @param  [String] state of the status. Can be: pending, running, success, failed, canceled
+    # @param  [Hash] options A customizable set of options.
+    # @option options [String] :ref The ref (branch or tag) to which the status refers
+    # @option options [String] :name Filer by status name, eg. jenkins
+    # @option options [String] :target_url The target URL to associate with this status
+    def update_commit_status(id, sha, state, options = {})
+      post("/projects/#{id}/statuses/#{sha}", query: options.merge(state: state))
+    end
+    alias_method :repo_update_commit_status, :update_commit_status
+  end
+end
--- spec/gitlab/client/repositories_spec.rb
+++ spec/gitlab/client/repositories_spec.rb
@@ -5,15 +5,8 @@ describe Gitlab::Client do
   it { should respond_to :repo_create_tag }
   it { should respond_to :repo_branches }
   it { should respond_to :repo_branch }
-  it { should respond_to :repo_commits }
-  it { should respond_to :repo_commit }
-  it { should respond_to :repo_commit_diff }
-  it { should respond_to :repo_commit_comments }
-  it { should respond_to :repo_create_commit_comment }
   it { should respond_to :repo_tree }
   it { should respond_to :repo_compare}
-  it { should respond_to :repo_commit_status }
-  it { should respond_to :repo_update_commit_status }
 
   describe ".tags" do
     before do
@@ -96,89 +89,6 @@ describe Gitlab::Client do
     end
   end
 
-  describe ".commits" do
-    before do
-      stub_get("/projects/3/repository/commits", "project_commits").
-        with(:query => {:ref_name => "api"})
-      @commits = Gitlab.commits(3, :ref_name => "api")
-    end
-
-    it "should get the correct resource" do
-      expect(a_get("/projects/3/repository/commits").
-        with(:query => {:ref_name => "api"})).to have_been_made
-    end
-
-    it "should return an array of repository commits" do
-      expect(@commits).to be_an Array
-      expect(@commits.first.id).to eq("f7dd067490fe57505f7226c3b54d3127d2f7fd46")
-    end
-  end
-
-  describe ".commit" do
-    before do
-      stub_get("/projects/3/repository/commits/6104942438c14ec7bd21c6cd5bd995272b3faff6", "project_commit")
-      @commit = Gitlab.commit(3, '6104942438c14ec7bd21c6cd5bd995272b3faff6')
-    end
-
-    it "should get the correct resource" do
-      expect(a_get("/projects/3/repository/commits/6104942438c14ec7bd21c6cd5bd995272b3faff6"))
-        .to have_been_made
-    end
-
-    it "should return a repository commit" do
-      expect(@commit.id).to eq("6104942438c14ec7bd21c6cd5bd995272b3faff6")
-    end
-  end
-
-  describe ".commit_diff" do
-    before do
-      stub_get("/projects/3/repository/commits/6104942438c14ec7bd21c6cd5bd995272b3faff6/diff", "project_commit_diff")
-      @diff = Gitlab.commit_diff(3, '6104942438c14ec7bd21c6cd5bd995272b3faff6')
-    end
-
-    it "should get the correct resource" do
-      expect(a_get("/projects/3/repository/commits/6104942438c14ec7bd21c6cd5bd995272b3faff6/diff"))
-        .to have_been_made
-    end
-
-    it "should return a diff of a commit" do
-      expect(@diff.new_path).to eq("doc/update/5.4-to-6.0.md")
-    end
-  end
-
-  describe ".commit_comments" do
-    before do
-      stub_get("/projects/3/repository/commits/6104942438c14ec7bd21c6cd5bd995272b3faff6/comments", "project_commit_comments")
-      @commit_comments = Gitlab.commit_comments(3, '6104942438c14ec7bd21c6cd5bd995272b3faff6')
-    end
-
-    it "should get the correct resource" do
-      expect(a_get("/projects/3/repository/commits/6104942438c14ec7bd21c6cd5bd995272b3faff6/comments"))
-        .to have_been_made
-    end
-
-    it "should return commit's comments" do
-      expect(@commit_comments).to be_an Array
-      expect(@commit_comments.length).to eq(2)
-      expect(@commit_comments[0].note).to eq("this is the 1st comment on commit 6104942438c14ec7bd21c6cd5bd995272b3faff6")
-      expect(@commit_comments[0].author.id).to eq(11)
-      expect(@commit_comments[1].note).to eq("another discussion point on commit 6104942438c14ec7bd21c6cd5bd995272b3faff6")
-      expect(@commit_comments[1].author.id).to eq(12)
-    end
-  end
-
-  describe ".create_commit_comment" do
-    before do
-      stub_post("/projects/3/repository/commits/6104942438c14ec7bd21c6cd5bd995272b3faff6/comments", "project_commit_comment")
-      @merge_request = Gitlab.create_commit_comment(3, '6104942438c14ec7bd21c6cd5bd995272b3faff6', 'Nice code!')
-    end
-
-    it "should return information about the newly created comment" do
-      expect(@merge_request.note).to eq('Nice code!')
-      expect(@merge_request.author.id).to eq(1)
-    end
-  end
-
   describe ".compare" do
     before do
       stub_get("/projects/3/repository/compare", "compare_merge_request_diff").
@@ -194,49 +104,6 @@ describe Gitlab::Client do
     it "should get diffs of a merge request" do
       expect(@diff.diffs).to be_kind_of Array
       expect(@diff.diffs.last["new_path"]).to eq "files/js/application.js"
-    end
-  end
-
-  describe ".commit_status" do
-    before do
-      stub_get("/projects/6/repository/commits/7d938cb8ac15788d71f4b67c035515a160ea76d8/statuses", 'project_commit_status').
-        with(query: { all: 'true' })
-      @statuses = Gitlab.commit_status(6, '7d938cb8ac15788d71f4b67c035515a160ea76d8', all: true)
-    end
-
-    it "should get the correct resource" do
-      expect(a_get("/projects/6/repository/commits/7d938cb8ac15788d71f4b67c035515a160ea76d8/statuses").
-        with(query: {all: true}))
-    end
-
-    it "should get statuses of a commit" do
-      expect(@statuses).to be_kind_of Array
-      expect(@statuses.first.sha).to eq('7d938cb8ac15788d71f4b67c035515a160ea76d8')
-      expect(@statuses.first.ref).to eq('decreased-spec')
-      expect(@statuses.first.status).to eq('failed')
-      expect(@statuses.last.sha).to eq('7d938cb8ac15788d71f4b67c035515a160ea76d8')
-      expect(@statuses.last.status).to eq('success')
-    end
-  end
-
-  describe ".update_commit_status" do
-    before do
-      stub_post("/projects/6/statuses/7d938cb8ac15788d71f4b67c035515a160ea76d8", 'project_update_commit_status').
-        with(query: {name: 'test', ref: 'decreased-spec', state: 'failed'})
-      @status = Gitlab.update_commit_status(6, '7d938cb8ac15788d71f4b67c035515a160ea76d8', 'failed', name: 'test', ref: 'decreased-spec')
-    end
-
-    it "should get the correct resource" do
-      expect(a_post('/projects/6/statuses/7d938cb8ac15788d71f4b67c035515a160ea76d8').
-        with(query: {name: 'test', ref: 'decreased-spec', state: 'failed'}))
-    end
-
-    it "should information about the newly created status" do
-      expect(@status).to be_kind_of Gitlab::ObjectifiedHash
-      expect(@status.id).to eq(498)
-      expect(@status.sha).to eq('7d938cb8ac15788d71f4b67c035515a160ea76d8')
-      expect(@status.status).to eq('failed')
-      expect(@status.ref).to eq('decreased-spec')
     end
   end
 end
