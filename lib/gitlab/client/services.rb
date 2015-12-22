1c35c6d8efb96490d3f1e4196632767287b39ee8
Azamat Khudaygulov <azamat.hudaygulov@daturum.ru>
2015-12-22 10:32:05 +0300

    Added possibility to change services in the project

---
+++ lib/gitlab/client/services.rb
@@ -0,0 +1,48 @@
+class Gitlab::Client
+  module Services
+    # Create/Edit service
+    # Full service params documentation: https://github.com/gitlabhq/gitlabhq/blob/master/doc/api/services.md
+    #
+    # @example
+    #   Gitlab.change_service(42, :redmine, new_issue_url: 'https://example.com/projects/test_project/issues/new',
+    #                                       project_url: 'https://example.com/projects/test_project/issues',
+    #                                       issues_url: 'https://example.com/issues/:id')
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [String] service A service code name.
+    # @param  [Hash] params A service parameters.
+    # @return [Boolean]
+    def change_service(project, service, params)
+      put("/projects/#{project}/services/#{correct_service_name(service)}", body: params)
+    end
+
+    # Delete service
+    #
+    # @example
+    #   Gitlab.delete_service(42, :redmine)
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [String] service A service code name.
+    # @return [Boolean]
+    def delete_service(project, service)
+      delete("/projects/#{project}/services/#{correct_service_name(service)}")
+    end
+
+    # Get service
+    #
+    # @example
+    #   Gitlab.service(42, :redmine)
+    #
+    # @param  [Integer] project The ID of a project.
+    # @param  [String] service A service code name.
+    # @return [Gitlab::ObjectifiedHash]
+    def service(project, service)
+      get("/projects/#{project}/services/#{correct_service_name(service)}")
+    end
+
+    private
+    def correct_service_name(service)
+      service.to_s.gsub('_', '-')
+    end
+  end
+end
---
+++ spec/gitlab/client/services_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe Gitlab::Client do
+  describe ".service" do
+    before do
+      stub_get("/projects/3/services/redmine", "service")
+      @service = Gitlab.service(3, :redmine)
+    end
+
+    it "should get the correct resource" do
+      expect(a_get("/projects/3/services/redmine")).to have_been_made
+    end
+
+    it "should return a information about a service of project" do
+      expect(@service.id).to eq 38
+      expect(@service.title).to eq("Redmine")
+      expect(@service.properties.project_url).to eq("https://example.com/projects/test_project/issue")
+    end
+  end
+
+  describe ".change_service" do
+    before do
+      stub_put("/projects/3/services/redmine", "service")
+      @service = Gitlab.change_service(3, :redmine, new_issue_url: 'https://example.com/projects/test_project/issues/new',
+                                                    project_url: 'https://example.com/projects/test_project/issues',
+                                                    issues_url: 'https://example.com/issues/:id')
+    end
+
+    it "should get the correct resource" do
+      body = {new_issue_url: 'https://example.com/projects/test_project/issues/new',
+              project_url: 'https://example.com/projects/test_project/issues',
+              issues_url: 'https://example.com/issues/:id'}
+      expect(a_put("/projects/3/services/redmine").with(body: body)).to have_been_made
+    end
+
+    it "should return information about a new snippet" do
+      expect(@service).to be_truthy
+    end
+  end
+
+  describe ".delete_servoce" do
+    before do
+      stub_delete("/projects/3/services/redmine", "service")
+      @service = Gitlab.delete_service(3, :redmine)
+    end
+
+    it "should get the correct resource" do
+      expect(a_delete("/projects/3/services/redmine")).to have_been_made
+    end
+
+    it "should return information about a deleted snippet" do
+      expect(@service).to be_truthy
+    end
+  end
+end
