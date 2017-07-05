23283f1010b212c64ea96d2389fc4193803204aa
akkee <akashsrvstv@yahoo.in>
2017-07-05 05:31:18 +0530

    Award Emoji API (#297)

---
+++ lib/gitlab/client/award_emojis.rb
@@ -0,0 +1,135 @@
+class Gitlab::Client
+  # Defines methods related to Award Emojis.
+  # @see https://docs.gitlab.com/ce/api/award_emoji.html
+  module AwardEmojis
+    # Gets a list of all award emoji for an awardable(issue, merge request or snippet)
+    #
+    # @example
+    #   Gitlab.award_emojis(1, 80, 'issue')
+    #   Gitlab.award_emojis(1, 60, 'merge_request')
+    #   Gitlab.award_emojis(1, 40, 'snippet')
+    #
+    # @param [Integer] project The ID of a project.
+    # @param [Integer] awardable_id The ID of an awardable(issue, merge request or snippet).
+    # @param [String] awardable_type The type of the awardable(can be 'issue', 'merge_request' or 'snippet')
+    # @return [Array<Gitlab::ObjectifiedHash>]
+    def award_emojis(project, awardable_id, awardable_type)
+      get("/projects/#{url_encode project}/#{awardable_type}s/#{awardable_id}/award_emoji")
+    end
+
+    # Gets a list of all award emoji for a single note on an awardable(issue, merge request or snippet)
+    #
+    # @example
+    #   Gitlab.note_award_emojis(1, 80, 'issue', 1)
+    #   Gitlab.note_award_emojis(1, 60, 'merge_request', 1)
+    #   Gitlab.note_award_emojis(1, 40, 'snippet', 1)
+    #
+    # @param [Integer] project The ID of a project.
+    # @param [Integer] awardable_id The ID of an awardable(issue, merge request or snippet).
+    # @param [String] awardable_type The type of the awardable(can be 'issue', 'merge_request' or 'snippet')
+    # @param [Integer] note_id The ID of a note.
+    # @return [Array<Gitlab::ObjectifiedHash>]
+    def note_award_emojis(project, awardable_id, awardable_type, note_id)
+      get("/projects/#{url_encode project}/#{awardable_type}s/#{awardable_id}/notes/#{note_id}/award_emoji")
+    end
+
+    # Gets a single award emoji for an awardable(issue, merge request or snippet)
+    #
+    # @example
+    #   Gitlab.award_emoji(1, 80, 'issue', 4)
+    #   Gitlab.award_emoji(1, 60, 'merge_request', 4)
+    #   Gitlab.award_emoji(1, 40, 'snippet', 4)
+    #
+    # @param [Integer] project The ID of a project.
+    # @param [Integer] awardable_id The ID of an awardable(issue, merge request or snippet).
+    # @param [String] awardable_type The type of the awardable(can be 'issue', 'merge_request' or 'snippet')
+    # @param [Integer] award_id The ID of an award emoji.
+    # @return [Gitlab::ObjectifiedHash]
+    def award_emoji(project, awardable_id, awardable_type, award_id)
+      get("/projects/#{url_encode project}/#{awardable_type}s/#{awardable_id}/award_emoji/#{award_id}")
+    end
+
+    # Gets a single award emoji from a single note on an awardable(issue, merge request or snippet)
+    #
+    # @example
+    #   Gitlab.note_award_emoji(1, 80, 'issue', 1, 4)
+    #   Gitlab.note_award_emoji(1, 60, 'merge_request', 1, 4)
+    #   Gitlab.note_award_emoji(1, 40, 'snippet', 1, 4)
+    #
+    # @param [Integer] project The ID of a project.
+    # @param [Integer] awardable_id The ID of an awardable(issue, merge request or snippet).
+    # @param [String] awardable_type The type of the awardable(can be 'issue', 'merge_request' or 'snippet')
+    # @param [Integer] note_id The ID of a note.
+    # @param [Integer] award_id The ID of an award emoji.
+    # @return [Gitlab::ObjectifiedHash]
+    def note_award_emoji(project, awardable_id, awardable_type, note_id, award_id)
+      get("/projects/#{url_encode project}/#{awardable_type}s/#{awardable_id}/notes/#{note_id}/award_emoji/#{award_id}")
+    end
+
+    # Awards a new emoji to an awardable(issue, merge request or snippet)
+    #
+    # @example
+    #   Gitlab.create_award_emoji(1, 80, 'issue', 'blowfish')
+    #   Gitlab.create_award_emoji(1, 80, 'merge_request', 'blowfish')
+    #   Gitlab.create_award_emoji(1, 80, 'snippet', 'blowfish')
+    #
+    # @param [Integer] project The ID of a project.
+    # @param [Integer] awardable_id The ID of an awardable(issue, merge request or snippet).
+    # @param [String] awardable_type The type of the awardable(can be 'issue', 'merge_request' or 'snippet')
+    # @param [String] emoji_name The name of the emoji, without colons.
+    # @return [Gitlab::ObjectifiedHash]
+    def create_award_emoji(project, awardable_id, awardable_type, emoji_name)
+      post("/projects/#{url_encode project}/#{awardable_type}s/#{awardable_id}/award_emoji", body: {name: emoji_name})
+    end
+
+    # Awards a new emoji to a note on an awardable(issue, merge request or snippet)
+    #
+    # @example
+    #   Gitlab.create_note_award_emoji(1, 80, 'issue', 1, 'blowfish')
+    #   Gitlab.create_note_award_emoji(1, 80, 'merge_request', 1, 'blowfish')
+    #   Gitlab.create_note_award_emoji(1, 80, 'snippet', 1, 'blowfish')
+    #
+    # @param [Integer] project The ID of a project.
+    # @param [Integer] awardable_id The ID of an awardable(issue, merge request or snippet).
+    # @param [String] awardable_type The type of the awardable(can be 'issue', 'merge_request' or 'snippet')
+    # @param [Integer] note_id The ID of a note.
+    # @param [String] emoji_name The name of the emoji, without colons.
+    # @return [Gitlab::ObjectifiedHash]
+    def create_note_award_emoji(project, awardable_id, awardable_type, note_id, emoji_name)
+      post("/projects/#{url_encode project}/#{awardable_type}s/#{awardable_id}/notes/#{note_id}/award_emoji", body: {name: emoji_name})
+    end
+
+    # Deletes a single award emoji from an awardable(issue, merge request or snippet)
+    #
+    # @example
+    #   Gitlab.delete_award_emoji(1, 80, 'issue', 4)
+    #   Gitlab.delete_award_emoji(1, 60, 'merge_request', 4)
+    #   Gitlab.delete_award_emoji(1, 40, 'snippet', 4)
+    #
+    # @param [Integer] project The ID of a project.
+    # @param [Integer] awardable_id The ID of an awardable(issue, merge request or snippet).
+    # @param [String] awardable_type The type of the awardable(can be 'issue', 'merge_request' or 'snippet')
+    # @param [Integer] award_id The ID of an award emoji.
+    # @return [void] This API call returns an empty response body.
+    def delete_award_emoji(project, awardable_id, awardable_type, award_id)
+      delete("/projects/#{url_encode project}/#{awardable_type}s/#{awardable_id}/award_emoji/#{award_id}")
+    end
+
+    # Deletes a single award emoji from a single note on an awardable(issue, merge request or snippet)
+    #
+    # @example
+    #   Gitlab.delete_note_award_emoji(1, 80, 'issue', 1, 4)
+    #   Gitlab.delete_note_award_emoji(1, 60, 'merge_request', 1, 4)
+    #   Gitlab.delete_note_award_emoji(1, 40, 'snippet', 1, 4)
+    #
+    # @param [Integer] project The ID of a project.
+    # @param [Integer] awardable_id The ID of an awardable(issue, merge request or snippet).
+    # @param [String] awardable_type The type of the awardable(can be 'issue', 'merge_request' or 'snippet')
+    # @param [Integer] note_id The ID of a note.
+    # @param [Integer] award_id The ID of an award emoji.
+    # @return [void] This API call returns an empty response body.
+    def delete_note_award_emoji(project, awardable_id, awardable_type, note_id, award_id)
+      delete("/projects/#{url_encode project}/#{awardable_type}s/#{awardable_id}/notes/#{note_id}/award_emoji/#{award_id}")
+    end
+  end
+end
---
+++ spec/gitlab/client/award_emojis_spec.rb
@@ -0,0 +1,391 @@
+require 'spec_helper'
+
+describe Gitlab::Client do
+  describe '.award_emojis' do
+    context 'when issue award emojis' do
+      before do
+        stub_get("/projects/1/issues/80/award_emoji", "issue_award_emojis")
+        @emojis = Gitlab.award_emojis(1, 80, 'issue') 
+      end
+
+      it "should get the correct resources" do
+        expect(a_get("/projects/1/issues/80/award_emoji")).to have_been_made
+      end
+
+      it "should return a paginated response of issue award emojis" do
+        expect(@emojis).to be_a Gitlab::PaginatedResponse
+        expect(@emojis.first.awardable_id).to eq(80)
+        expect(@emojis.first.awardable_type).to eq("Issue")
+      end
+    end
+
+    context 'when merge request award emojis' do
+      before do
+        stub_get("/projects/1/merge_requests/80/award_emoji", "merge_request_award_emojis")
+        @emojis = Gitlab.award_emojis(1, 80, 'merge_request') 
+      end
+
+      it "should get the correct resources" do
+        expect(a_get("/projects/1/merge_requests/80/award_emoji")).to have_been_made
+      end
+
+      it "should return a paginated response of merge request award emojis" do
+        expect(@emojis).to be_a Gitlab::PaginatedResponse
+        expect(@emojis.first.awardable_id).to eq(80)
+        expect(@emojis.first.awardable_type).to eq("MergeRequest")
+      end
+    end
+
+    context 'when snippet award emojis' do
+      before do
+        stub_get("/projects/1/snippets/80/award_emoji", "snippet_award_emojis")
+        @emojis = Gitlab.award_emojis(1, 80, 'snippet') 
+      end
+
+      it "should get the correct resources" do
+        expect(a_get("/projects/1/snippets/80/award_emoji")).to have_been_made
+      end
+
+      it "should return a paginated response of snippet award emojis" do
+        expect(@emojis).to be_a Gitlab::PaginatedResponse
+        expect(@emojis.first.awardable_id).to eq(80)
+        expect(@emojis.first.awardable_type).to eq("Snippet")
+      end
+    end
+  end
+
+  describe '.note_award_emojis' do
+    context 'when issue note award emojis' do
+      before do
+        stub_get("/projects/1/issues/80/notes/1/award_emoji", "note_award_emojis")
+        @note_emojis = Gitlab.note_award_emojis(1, 80, 'issue', 1) 
+      end
+
+      it "should get the correct resources" do
+        expect(a_get("/projects/1/issues/80/notes/1/award_emoji")).to have_been_made
+      end
+
+      it "should return a paginated response of issue note award emojis" do
+        expect(@note_emojis).to be_a Gitlab::PaginatedResponse
+        expect(@note_emojis.first.awardable_id).to eq(1)
+        expect(@note_emojis.first.awardable_type).to eq("Note")
+      end
+    end
+
+    context 'when merge request note award emojis' do
+      before do
+        stub_get("/projects/1/merge_requests/80/notes/1/award_emoji", "note_award_emojis")
+        @note_emojis = Gitlab.note_award_emojis(1, 80, 'merge_request', 1) 
+      end
+
+      it "should get the correct resources" do
+        expect(a_get("/projects/1/merge_requests/80/notes/1/award_emoji")).to have_been_made
+      end
+
+      it "should return a paginated response of merge request note award emojis" do
+        expect(@note_emojis).to be_a Gitlab::PaginatedResponse
+        expect(@note_emojis.first.awardable_id).to eq(1)
+        expect(@note_emojis.first.awardable_type).to eq("Note")
+      end
+    end
+
+    context 'when snippet note award emojis' do
+      before do
+        stub_get("/projects/1/snippets/80/notes/1/award_emoji", "note_award_emojis")
+        @note_emojis = Gitlab.note_award_emojis(1, 80, 'snippet', 1) 
+      end
+
+      it "should get the correct resources" do
+        expect(a_get("/projects/1/snippets/80/notes/1/award_emoji")).to have_been_made
+      end
+
+      it "should return a paginated response of snippet note award emojis" do
+        expect(@note_emojis).to be_a Gitlab::PaginatedResponse
+        expect(@note_emojis.first.awardable_id).to eq(1)
+        expect(@note_emojis.first.awardable_type).to eq("Note")
+      end
+    end
+  end
+
+  describe '.award_emoji' do
+    context 'when issue award emoji' do
+      before do
+        stub_get("/projects/1/issues/80/award_emoji/4", "issue_award_emoji")
+        @emoji = Gitlab.award_emoji(1, 80, 'issue', 4) 
+      end
+
+      it "should get the correct resource" do
+        expect(a_get("/projects/1/issues/80/award_emoji/4")).to have_been_made
+      end
+
+      it "should return information about an issue award emoji" do
+        expect(@emoji.id).to eq(4)
+        expect(@emoji.awardable_type).to eq("Issue")
+        expect(@emoji.awardable_id).to eq(80)
+      end
+    end
+
+    context 'when merge request award emoji' do
+      before do
+        stub_get("/projects/1/merge_requests/80/award_emoji/4", "merge_request_award_emoji")
+        @emoji = Gitlab.award_emoji(1, 80, 'merge_request', 4) 
+      end
+
+      it "should get the correct resource" do
+        expect(a_get("/projects/1/merge_requests/80/award_emoji/4")).to have_been_made
+      end
+
+      it "should return information about a merge request award emoji" do
+        expect(@emoji.id).to eq(4)
+        expect(@emoji.awardable_type).to eq("MergeRequest")
+        expect(@emoji.awardable_id).to eq(80)
+      end
+    end
+
+    context 'when snippet award emoji' do
+      before do
+        stub_get("/projects/1/snippets/80/award_emoji/4", "snippet_award_emoji")
+        @emoji = Gitlab.award_emoji(1, 80, 'snippet', 4) 
+      end
+
+      it "should get the correct resource" do
+        expect(a_get("/projects/1/snippets/80/award_emoji/4")).to have_been_made
+      end
+
+      it "should return information about a snippet award emoji" do
+        expect(@emoji.id).to eq(4)
+        expect(@emoji.awardable_type).to eq("Snippet")
+        expect(@emoji.awardable_id).to eq(80)
+      end
+    end
+  end
+
+  describe '.note_award_emoji' do
+    context 'when issue note award emoji' do
+      before do
+        stub_get("/projects/1/issues/80/notes/1/award_emoji/4", "note_award_emoji")
+        @note_emoji = Gitlab.note_award_emoji(1, 80, 'issue', 1, 4) 
+      end
+
+      it "should get the correct resource" do
+        expect(a_get("/projects/1/issues/80/notes/1/award_emoji/4")).to have_been_made
+      end
+
+      it "should return information about an issue note award emoji" do
+        expect(@note_emoji.id).to eq(4)
+        expect(@note_emoji.awardable_type).to eq("Note")
+        expect(@note_emoji.awardable_id).to eq(1)
+      end
+    end
+
+    context 'when merge request note award emoji' do
+      before do
+        stub_get("/projects/1/merge_requests/80/notes/1/award_emoji/4", "note_award_emoji")
+        @note_emoji = Gitlab.note_award_emoji(1, 80, 'merge_request', 1, 4) 
+      end
+
+      it "should get the correct resource" do
+        expect(a_get("/projects/1/merge_requests/80/notes/1/award_emoji/4")).to have_been_made
+      end
+
+      it "should return information about a merge request note award emoji" do
+        expect(@note_emoji.id).to eq(4)
+        expect(@note_emoji.awardable_type).to eq("Note")
+        expect(@note_emoji.awardable_id).to eq(1)
+      end
+    end
+
+    context 'when snippet note award emoji' do
+      before do
+        stub_get("/projects/1/snippets/80/notes/1/award_emoji/4", "note_award_emoji")
+        @note_emoji = Gitlab.note_award_emoji(1, 80, 'snippet', 1, 4) 
+      end
+
+      it "should get the correct resource" do
+        expect(a_get("/projects/1/snippets/80/notes/1/award_emoji/4")).to have_been_made
+      end
+
+      it "should return information about a snippet note award emoji" do
+        expect(@note_emoji.id).to eq(4)
+        expect(@note_emoji.awardable_type).to eq("Note")
+        expect(@note_emoji.awardable_id).to eq(1)
+      end
+    end
+  end
+
+  describe '.create_award_emoji' do
+    context 'when issue award emoji' do
+      before do
+        stub_post("/projects/1/issues/80/award_emoji", "issue_award_emoji")
+        @emoji = Gitlab.create_award_emoji(1, 80, "issue", "blowfish")
+      end
+
+      it "should get the correct resource" do
+        expect(a_post("/projects/1/issues/80/award_emoji").
+          with(body: { name: 'blowfish' })).to have_been_made
+      end
+
+      it "should return correct information about the created issue award emoji" do
+        expect(@emoji.name).to eq('blowfish')
+        expect(@emoji.awardable_type).to eq('Issue')
+      end
+    end
+
+    context 'when merge request award emoji' do
+      before do
+        stub_post("/projects/1/merge_requests/80/award_emoji", "merge_request_award_emoji")
+        @emoji = Gitlab.create_award_emoji(1, 80, "merge_request", "blowfish")
+      end
+
+      it "should get the correct resource" do
+        expect(a_post("/projects/1/merge_requests/80/award_emoji").
+          with(body: { name: 'blowfish' })).to have_been_made
+      end
+
+      it "should return correct information about the created merge request award emoji" do
+        expect(@emoji.name).to eq('blowfish')
+        expect(@emoji.awardable_type).to eq('MergeRequest')
+      end
+    end
+
+    context 'when snippet award emoji' do
+      before do
+        stub_post("/projects/1/snippets/80/award_emoji", "snippet_award_emoji")
+        @emoji = Gitlab.create_award_emoji(1, 80, "snippet", "blowfish")
+      end
+
+      it "should get the correct resource" do
+        expect(a_post("/projects/1/snippets/80/award_emoji").
+          with(body: { name: 'blowfish' })).to have_been_made
+      end
+
+      it "should return correct information about the created snippet award emoji" do
+        expect(@emoji.name).to eq('blowfish')
+        expect(@emoji.awardable_type).to eq('Snippet')
+      end
+    end
+  end
+
+  describe '.create_note_award_emoji' do
+    context 'when issue note award emoji' do
+      before do
+        stub_post("/projects/1/issues/80/notes/1/award_emoji", "note_award_emoji")
+        @note_emoji = Gitlab.create_note_award_emoji(1, 80, "issue", 1, "mood_bubble_lightning")
+      end
+
+      it "should get the correct resource" do
+        expect(a_post("/projects/1/issues/80/notes/1/award_emoji").
+          with(body: { name: 'mood_bubble_lightning' })).to have_been_made
+      end
+
+      it "should return correct information about the created issue note award emoji" do
+        expect(@note_emoji.name).to eq('mood_bubble_lightning')
+        expect(@note_emoji.awardable_type).to eq('Note')
+      end
+    end
+
+    context 'when merge request note award emoji' do
+      before do
+        stub_post("/projects/1/merge_requests/80/notes/1/award_emoji", "note_award_emoji")
+        @note_emoji = Gitlab.create_note_award_emoji(1, 80, "merge_request", 1, "mood_bubble_lightning")
+      end
+
+      it "should get the correct resource" do
+        expect(a_post("/projects/1/merge_requests/80/notes/1/award_emoji").
+          with(body: { name: 'mood_bubble_lightning' })).to have_been_made
+      end
+
+      it "should return correct information about the created merge request note award emoji" do
+        expect(@note_emoji.name).to eq('mood_bubble_lightning')
+        expect(@note_emoji.awardable_type).to eq('Note')
+      end
+    end
+
+    context 'when snippet note award emoji' do
+      before do
+        stub_post("/projects/1/snippets/80/notes/1/award_emoji", "note_award_emoji")
+        @note_emoji = Gitlab.create_note_award_emoji(1, 80, "snippet", 1, "mood_bubble_lightning")
+      end
+
+      it "should get the correct resource" do
+        expect(a_post("/projects/1/snippets/80/notes/1/award_emoji").
+          with(body: { name: 'mood_bubble_lightning' })).to have_been_made
+      end
+
+      it "should return correct information about the created snippet note award emoji" do
+        expect(@note_emoji.name).to eq('mood_bubble_lightning')
+        expect(@note_emoji.awardable_type).to eq('Note')
+      end
+    end
+  end
+
+  describe '.delete_award_emoji' do
+    context 'when issue award emoji' do
+      before do
+        stub_delete("/projects/1/issues/80/award_emoji/4", "issue_award_emoji")
+        @emoji = Gitlab.delete_award_emoji(1, 80, "issue", 4)
+      end
+
+      it "should get the correct resource" do
+        expect(a_delete("/projects/1/issues/80/award_emoji/4")).to have_been_made
+      end
+    end
+
+    context 'when merge request award emoji' do
+      before do
+        stub_delete("/projects/1/merge_requests/80/award_emoji/4", "merge_request_award_emoji")
+        @emoji = Gitlab.delete_award_emoji(1, 80, "merge_request", 4)
+      end
+
+      it "should get the correct resource" do
+        expect(a_delete("/projects/1/merge_requests/80/award_emoji/4")).to have_been_made
+      end
+    end
+
+    context 'when snippet award emoji' do
+      before do
+        stub_delete("/projects/1/snippets/80/award_emoji/4", "snippet_award_emoji")
+        @emoji = Gitlab.delete_award_emoji(1, 80, "snippet", 4)
+      end
+
+      it "should get the correct resource" do
+        expect(a_delete("/projects/1/snippets/80/award_emoji/4")).to have_been_made
+      end
+    end
+  end
+
+  describe '.delete_note_award_emoji' do
+    context 'when issue note award emoji' do
+      before do
+        stub_delete("/projects/1/issues/80/notes/1/award_emoji/4", "note_award_emoji")
+        @note_emoji = Gitlab.delete_note_award_emoji(1, 80, "issue", 1, 4)
+      end
+
+      it "should get the correct resource" do
+        expect(a_delete("/projects/1/issues/80/notes/1/award_emoji/4")).to have_been_made
+      end
+    end
+
+    context 'when merge request note award emoji' do
+      before do
+        stub_delete("/projects/1/merge_requests/80/notes/1/award_emoji/4", "note_award_emoji")
+        @note_emoji = Gitlab.delete_note_award_emoji(1, 80, "merge_request", 1, 4)
+      end
+
+      it "should get the correct resource" do
+        expect(a_delete("/projects/1/merge_requests/80/notes/1/award_emoji/4")).to have_been_made
+      end
+    end
+
+    context 'when snippet note award emoji' do
+      before do
+        stub_delete("/projects/1/snippets/80/notes/1/award_emoji/4", "note_award_emoji")
+        @note_emoji = Gitlab.delete_note_award_emoji(1, 80, "snippet", 1, 4)
+      end
+
+      it "should get the correct resource" do
+        expect(a_delete("/projects/1/snippets/80/notes/1/award_emoji/4")).to have_been_made
+      end
+    end
+  end
+end
