119a99a7ec2a0debce727521eecf14ef38b81fbc
Erwan Daubert <erwan.daubert@gmail.com>
2017-07-09 20:08:32 +0200

    replace `projects/search/{query}` by `projects?search={query}`

--- lib/gitlab/client/projects.rb
+++ lib/gitlab/client/projects.rb
@@ -34,7 +34,7 @@ class Gitlab::Client
     # @option options [String] :sort Return requests sorted in asc or desc order
     # @return [Array<Gitlab::ObjectifiedHash>]
     def project_search(query, options={})
-      get("/projects/search/#{query}", query: options)
+      get("/projects?search=#{query}", query: options)
     end
     alias_method :search_projects, :project_search
 
