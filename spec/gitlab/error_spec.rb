2eb3a093419fcf367cbd19576c116febfbd17d2e
Sean Edge <asedge@gmail.com>
2018-06-21 06:23:13 -0400

    Do not try to access an ObjectifiedHash with #[]. The method is curre… (#389)
    * Do not try to access an ObjectifiedHash with #[]. The method is currently undefined and when #method_missing is invoked, only expects 1 param, it is handed 2 parameters: [] and the key. This causes a confusing ArgumentError on #method_missing.
    
    * Rewrote the tests for Gitlab::Error::ResponseError.  I tried to simplify the set up for additional test cases so all that would be needed is a new response scenario and a new expected message.

--- spec/gitlab/error_spec.rb
+++ spec/gitlab/error_spec.rb
@@ -1,72 +1,31 @@
-require "spec_helper"
-require "stringio"
+require 'spec_helper'
 
-describe Gitlab::Error do
-  let(:request_object)  { HTTParty::Request.new(Net::HTTP::Get, '/') }
-  let(:response_object) { Net::HTTPOK.new('1.1', 200, 'OK') }
-  let(:body)            { StringIO.new("{foo:'bar'}") }
-  let(:parsed_response) { -> { body } }
-
-  let(:response) do
-    HTTParty::Response.new(
-      request_object,
-      response_object,
-      parsed_response,
-      body: body
-    )
-  end
-
-  let(:error) { Gitlab::Error::ResponseError.new(response) }
-  let(:date)  { Date.new(2010, 1, 15).to_s }
-
+describe Gitlab::Error::ResponseError do
   before do
-    def body.message
-      string
-    end
-
-    response_object['last-modified']  = date
-    response_object['content-length'] = "1024"
+    @request_double = double(base_uri: 'https://gitlab.com/api/v3', path: '/foo')
   end
 
-  describe "#handle_message" do
-    let(:array) { Array.new(['First message.', 'Second message.']) }
-    let(:obj_h) do
-      Gitlab::ObjectifiedHash.new(
-        user:         ['not set'],
-        password:     ['too short'],
-        embed_entity: { foo: ['bar'], sna: ['fu'] }
-      )
-    end
-
-    context "when passed an ObjectifiedHash" do
-      it "returns a joined string of error messages sorted by key" do
-        expect(error.send(:handle_message, obj_h)).
-          to eq(
-            "'embed_entity' (foo: bar) (sna: fu), 'password' too short, 'user' not set"
-          )
-      end
-    end
-
-    context "when passed an Array" do
-      it "returns a joined string of messages" do
-        expect(error.send(:handle_message, array)).
-          to eq("First message. Second message.")
-      end
-    end
-
-    context "when passed a String" do
-      it "returns the String untouched" do
-        error_str = 'this is an error string'
-
-        expect(error.send(:handle_message, error_str)).
-          to eq(error_str)
-      end
-    end
+  let(:expected_messages) do
+    [
+      %r{Server responded with code \d+, message: Displayed message. Request URI: https://gitlab.com/api/v3/foo},
+      %r{Server responded with code \d+, message: Displayed error_description. Request URI: https://gitlab.com/api/v3/foo},
+      %r{Server responded with code \d+, message: Displayed error. Request URI: https://gitlab.com/api/v3/foo},
+      %r{Server responded with code \d+, message: 'embed_entity' \(foo: bar\) \(sna: fu\), 'password' too short. Request URI: https://gitlab.com/api/v3/foo},
+      %r{Server responded with code \d+, message: First message. Second message.. Request URI: https://gitlab.com/api/v3/foo},
+    ]
   end
 
-  describe "#response_message" do
-    it "returns the message of the parsed_response" do
-      expect(error.response_message).to eq(body.string)
+  # Set up some response scenarios to test.
+  [
+    { code: 401, parsed_response: Gitlab::ObjectifiedHash.new(message: 'Displayed message', error_description: 'should not be displayed', error: 'also will not be displayed')},
+    { code: 404, parsed_response: Gitlab::ObjectifiedHash.new(error_description: 'Displayed error_description', error: 'also will not be displayed')},
+    { code: 401, parsed_response: Gitlab::ObjectifiedHash.new(error: 'Displayed error')},
+    { code: 500, parsed_response: Gitlab::ObjectifiedHash.new(embed_entity: { foo: ['bar'], sna: ['fu']}, password: ['too short'])},
+    { code: 403, parsed_response: Array.new(['First message.', 'Second message.'])},
+  ].each_with_index do |data, index|
+    it 'returns the expected message' do
+      response_double = double(**data, request: @request_double)
+      expect(described_class.new(response_double).message).to match expected_messages[index]
     end
   end
 end
