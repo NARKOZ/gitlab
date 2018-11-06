52aabeb4363e73ffcf5d72f8d1c464b75d9dd69a
Jan Krutisch <jan@krutisch.de>
2018-11-06 12:18:35 +0100

    Fixes a bug in handling "Spam detected" messages
    This handles error responses created by Gitlab.com's spam protection
    correctly. The error message is nested in a slightly off way (I believe
    this is a bug), causing the error message formatting code to blow up as
    it tries to join a string.

--- spec/gitlab/error_spec.rb
+++ spec/gitlab/error_spec.rb
@@ -13,7 +13,8 @@ describe Gitlab::Error::ResponseError do
       %r{Server responded with code \d+, message: Displayed error_description. Request URI: https://gitlab.com/api/v3/foo},
       %r{Server responded with code \d+, message: Displayed error. Request URI: https://gitlab.com/api/v3/foo},
       %r{Server responded with code \d+, message: 'embed_entity' \(foo: bar\) \(sna: fu\), 'password' too short. Request URI: https://gitlab.com/api/v3/foo},
-      %r{Server responded with code \d+, message: First message. Second message.. Request URI: https://gitlab.com/api/v3/foo}
+      %r{Server responded with code \d+, message: First message. Second message.. Request URI: https://gitlab.com/api/v3/foo},
+      %r{Server responded with code \d+, message: 'error' Spam detected. Request URI: https://gitlab.com/api/v3/foo}
     ]
   end
 
@@ -23,7 +24,9 @@ describe Gitlab::Error::ResponseError do
     { code: 404, parsed_response: Gitlab::ObjectifiedHash.new(error_description: 'Displayed error_description', error: 'also will not be displayed') },
     { code: 401, parsed_response: Gitlab::ObjectifiedHash.new(error: 'Displayed error') },
     { code: 500, parsed_response: Gitlab::ObjectifiedHash.new(embed_entity: { foo: ['bar'], sna: ['fu'] }, password: ['too short']) },
-    { code: 403, parsed_response: Array.new(['First message.', 'Second message.']) }
+    { code: 403, parsed_response: Array.new(['First message.', 'Second message.']) },
+    { code: 400, parsed_response: Gitlab::ObjectifiedHash.new(message: { error: 'Spam detected' } ) }
+
   ].each_with_index do |data, index|
     it 'returns the expected message' do
       response_double = double(**data, request: @request_double)
