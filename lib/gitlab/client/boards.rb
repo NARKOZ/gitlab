5f33da0248005a1ac34279772b3fdf75ad51236c
Akash Srivastava <akashsrvstv@yahoo.in>
2019-02-20 16:06:50 +0530

    Adding missing endpoints in project issue boards

--- lib/gitlab/client/boards.rb
+++ lib/gitlab/client/boards.rb
@@ -19,6 +19,62 @@ class Gitlab::Client
       get("/projects/#{url_encode project}/boards", query: options)
     end
 
+    # Get a single board.
+    #
+    # @example
+    #   Gitlab.board(5, 1)
+    #
+    # @param  [Integer, String] project The ID or name of a project.
+    # @param  [Integer] id The ID of a board.
+    # @return [Gitlab::ObjectifiedHash] Returns information about the board
+    def board(project, id)
+      get("/projects/#{url_encode project}/boards/#{id}")
+    end
+
+    # Creates a new board.
+    #
+    # @example
+    #   Gitlab.create_board(5, 'newboard')
+    #
+    # @param  [Integer, String] project The ID or name of a project.
+    # @param  [String] name The name of the new board.
+    # @return [Gitlab::ObjectifiedHash] Information about created board.
+    def create_board(project, name)
+      body = { name: name }
+      post("/projects/#{url_encode project}/boards", body: body)
+    end
+
+    # Updates a board.
+    #
+    # @example
+    #   Gitlab.edit_board(5, 1, name: 'new_name')
+    #   Gitlab.edit_board(5, 1, name: 'new_name', assignee_id: 1, milestone_id: 1)
+    #
+    # @param  [Integer, String] project The ID or name of a project.
+    # @param  [Integer] id The ID of a board.
+    # @param  [Hash] options A customizable set of options.
+    # @option options [String] :name(optional) The new name of the board.
+    # @option options [Integer] :assignee_id(optional) The assignee the board should be scoped to.
+    # @option options [Integer] :milestone_id(optional) The milestone the board should be scoped to.
+    # @option options [String] :labels(optional) Comma-separated list of label names which the board should be scoped to.
+    # @option options [Integer] :weight(optional) The weight range from 0 to 9, to which the board should be scoped to.
+    # @return [Gitlab::ObjectifiedHash] Information about updated board.
+    def edit_board(project, id, options = {})
+      put("/projects/#{url_encode project}/boards/#{id}", body: options)
+    end
+
+    # Deletes a board.
+    #
+    # @example
+    #   Gitlab.delete_board(5, 1)
+    #
+    # @param  [Integer, String] project The ID or name of a project.
+    # @param  [Integer] id The ID of a board.
+    # @return [void] This API call returns an empty response body.
+    def delete_board(project, id)
+      delete("/projects/#{url_encode project}/boards/#{id}")
+    end
+
     # Gets a board lists
     #
     # @example
