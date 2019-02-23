bd472d8a79bdd4ac154ac66a29fecb1b76cc414c
Akash Srivastava <akashsrvstv@yahoo.in>
2019-02-23 19:48:10 +0530

    Doc and argument fixes in markdown API

--- lib/gitlab/client/markdown.rb
+++ lib/gitlab/client/markdown.rb
@@ -2,21 +2,22 @@
 
 class Gitlab::Client
   # Defines methods related to markdown.
-  # @see https://docs.gitlab.com/ce/api/avatar.html
+  # @see https://docs.gitlab.com/ce/api/markdown.html
   module Markdown
     # Render an arbitrary Markdown document
     #
     # @example
-    #   Gitlab.markdown(text: 'Hello world! :tada:')
-    #   Gitlab.markdown(text: 'Hello world! :tada:', gfm: true, project: 'group_example/project_example')
+    #   Gitlab.markdown('Hello world! :tada:')
+    #   Gitlab.markdown('Hello world! :tada:', gfm: true, project: 'group_example/project_example')
     #
+    # @param  [String] text The markdown text to render.
     # @param  [Hash] options A customizable set of options.
-    # @option options [String] :text(required) The markdown text to render.
     # @option options [Boolean] :gfm(optional) Render text using GitLab Flavored Markdown. Default is false.
-    # @option options [String] :project(required) Use project as a context when creating references using GitLab Flavored Markdown. Authentication is required if a project is not public.
+    # @option options [String] :project(optional) Use project as a context when creating references using GitLab Flavored Markdown. Authentication is required if a project is not public.
     # @return <Gitlab::ObjectifiedHash> Returns the rendered markdown as response
-    def markdown(options = {})
-      post('/markdown', body: options)
+    def markdown(text, options = {})
+      body = { text: text }.merge(options)
+      post('/markdown', body: body)
     end
   end
 end
--- spec/gitlab/client/markdown_spec.rb
+++ spec/gitlab/client/markdown_spec.rb
@@ -6,7 +6,7 @@ describe Gitlab::Client do
   describe '.markdown' do
     before do
       stub_post('/markdown', 'markdown')
-      Gitlab.markdown(text: 'Hello world! :tada:', gfm: true, project: 'group_example/project_example')
+      Gitlab.markdown('Hello world! :tada:', gfm: true, project: 'group_example/project_example')
     end
 
     it 'gets the correct resource' do
