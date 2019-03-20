88b9618c8e284452ff1137d24e68cbdbe70ce73a
Quantu <673216+Quantu@users.noreply.github.com>
2019-03-20 11:15:20 -0400

    Fix edit_project_approvers
    Fix edit_project_approvers to use correct API path & update comments to properly reflect options as required, and to set to nil if you want one or both of them empty.

--- lib/gitlab/client/merge_request_approvals.rb
+++ lib/gitlab/client/merge_request_approvals.rb
@@ -37,11 +37,11 @@ class Gitlab::Client
     #    Gitlab.edit_project_approvers(1, {approver_ids: [5], approver_groups: [1]})
     #
     # @param [Integer] project(required) The ID of a project.
-    # @option options [Array] :approver_ids(optional) An array of User IDs that can approve MRs
-    # @option options [Array] :approver_group_ids(optional) An array of Group IDs whose members can approve MRs
+    # @option options [Array] :approver_ids(required, nil if none) An array of User IDs that can approve MRs
+    # @option options [Array] :approver_group_ids(required, nil if none) An array of Group IDs whose members can approve MRs
     # @return [Gitlab::ObjectifiedHash] MR approval configuration information about the project
     def edit_project_approvers(project, options = {})
-      put("/projects/#{url_encode project}/approvals", body: options)
+      put("/projects/#{url_encode project}/approvers", body: options)
     end
 
     # Get Configuration for approvals on a specific Merge Request.
