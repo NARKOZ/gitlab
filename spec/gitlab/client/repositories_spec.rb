f7df9767465f0a1ab6571ba47d8757a348500c82
johnbatty <john.batty@metaswitch.com>
2020-02-07 09:41:53 +0000

    Add repository contributors API (#547)
    * Add repository contributors API
    
    * Rubocop cleanup
    
    * Rubocop quote fix
    
    * Remove extra spaces from end of file

--- spec/gitlab/client/repositories_spec.rb
+++ spec/gitlab/client/repositories_spec.rb
@@ -9,6 +9,7 @@ describe Gitlab::Client do
   it { is_expected.to respond_to :repo_branch }
   it { is_expected.to respond_to :repo_tree }
   it { is_expected.to respond_to :repo_compare }
+  it { is_expected.to respond_to :repo_contributors }
 
   describe '.tags' do
     before do
@@ -110,5 +111,22 @@ describe Gitlab::Client do
       expect(@response).to be_kind_of Gitlab::ObjectifiedHash
       expect(@response.id).to eq '1a0b36b3cdad1d2ee32457c102a8c0b7056fa863'
     end
+  end
+end
+
+describe '.contributors' do
+  before do
+    stub_get('/projects/3/repository/contributors', 'contributors')
+    @contributors = Gitlab.contributors(3)
+  end
+
+  it 'gets the correct resource' do
+    expect(a_get('/projects/3/repository/contributors')).to have_been_made
+  end
+
+  it 'returns a paginated response of repository contributors' do
+    expect(@contributors).to be_a Gitlab::PaginatedResponse
+    expect(@contributors.first.name).to eq('Dmitriy Zaporozhets')
+    expect(@contributors.first.commits).to eq(117)
   end
 end
