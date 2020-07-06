a4d1c5d666f6cf78af7af86f667815cbf4295830
Nihad Abbasov <nihad@42na.in>
2020-07-07 00:04:20 +0400

    Expose validate_gitlab_ci_yml action
    Fixes #566

--- lib/gitlab.rb
+++ lib/gitlab.rb
@@ -42,7 +42,7 @@ module Gitlab
   #
   # @return [Array<Symbol>]
   def self.actions
-    hidden = /endpoint|private_token|auth_token|user_agent|sudo|get|post|put|\Adelete\z|validate|request_defaults|httparty/
+    hidden = /endpoint|private_token|auth_token|user_agent|sudo|get|post|put|\Adelete\z|validate\z|request_defaults|httparty/
     (Gitlab::Client.instance_methods - Object.methods).reject { |e| e[hidden] }
   end
 end
