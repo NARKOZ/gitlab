d7da83de6d739378f980de1db606ffe93916f124
Pim Snel <pim@lingewoud.nl>
2020-12-17 20:21:48 +0100

    new def and spec: key_by_fingerprint

--- lib/gitlab/client/keys.rb
+++ lib/gitlab/client/keys.rb
@@ -4,6 +4,7 @@ class Gitlab::Client
   # Defines methods related to keys.
   # @see https://docs.gitlab.com/ce/api/keys.html
   module Keys
+
     # Gets information about a key.
     #
     # @example
@@ -13,6 +14,17 @@ class Gitlab::Client
     # @return [Gitlab::ObjectifiedHash]
     def key(id)
       get("/keys/#{id}")
+    end
+
+    # Gets information about a key by key fingerprint.
+    #
+    # @example
+    #   Gitlab.key_by_fingerprint("9f:70:33:b3:50:4d:9a:a3:ef:ea:13:9b:87:0f:7f:7e")
+    #
+    # @param  [String] fingerprint The Fingerprint of a key.
+    # @return [Gitlab::ObjectifiedHash]
+    def key_by_fingerprint(fingerprint)
+      get('/keys/', query: {fingerprint: fingerprint})
     end
   end
 end
--- spec/gitlab/client/keys_spec.rb
+++ spec/gitlab/client/keys_spec.rb
@@ -18,4 +18,21 @@ describe Gitlab::Client do
       expect(@key.title).to eq('narkoz@helium')
     end
   end
+
+  describe '.key_by_fingerprint' do
+    before do
+      stub_get('/keys/?fingerprint=9f:70:33:b3:50:4d:9a:a3:ef:ea:13:9b:87:0f:7f:7e', 'key')
+      @key = Gitlab.key_by_fingerprint("9f:70:33:b3:50:4d:9a:a3:ef:ea:13:9b:87:0f:7f:7e")
+    end
+
+    it 'gets the correct resource' do
+      expect(a_get('/keys/?fingerprint=9f:70:33:b3:50:4d:9a:a3:ef:ea:13:9b:87:0f:7f:7e')).to have_been_made
+    end
+
+    it 'returns information about a key' do
+      expect(@key.id).to eq(1)
+      expect(@key.title).to eq('narkoz@helium')
+    end
+  end
+
 end
