a66d039e625258f2facc175a5df3d1a850aea665
Pim Snel <pim@lingewoud.nl>
2020-12-31 18:43:04 +0100

    Feature find user/key by fingerprint (#596)
    * new def and spec: key_by_fingerprint
    
    * fix rubocop offences
    
    * remove trailing slash

--- lib/gitlab/client/keys.rb
+++ lib/gitlab/client/keys.rb
@@ -14,5 +14,16 @@ class Gitlab::Client
     def key(id)
       get("/keys/#{id}")
     end
+
+    # Gets information about a key by key fingerprint.
+    #
+    # @example
+    #   Gitlab.key_by_fingerprint("9f:70:33:b3:50:4d:9a:a3:ef:ea:13:9b:87:0f:7f:7e")
+    #
+    # @param  [String] fingerprint The Fingerprint of a key.
+    # @return [Gitlab::ObjectifiedHash]
+    def key_by_fingerprint(fingerprint)
+      get('/keys', query: { fingerprint: fingerprint })
+    end
   end
 end
